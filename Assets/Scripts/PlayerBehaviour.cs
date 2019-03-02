396a562d0df55ada0b2dab5ae985505a6d525518
Unknown <f.noel@rubika-edu.com>
2019-03-02 17:21:10 +0100

    BlindPath
    Ce qui distingue le BlindPath de Vibration c'est le Booléen "use Vibration" et ouai c'est marrant
    
    Je le jure que je jure de faire la condition de victoire et le timer pour le blind Path aujourd'hui ... sur la vie de la tête d'un Alien

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using XInputDotNetPure;
 using UnityEngine.Tilemaps;
+using Cinemachine;
 
 
 public class PlayerBehaviour : MonoBehaviour
 {
-
+    [Header("Récupération des components")]
     public Rigidbody2D rb;
-    public Tilemap tilemapVictory;
-    public TilemapRenderer whiteTilemap;
-    public Camera cam;
-    private Vector2 direction;
 
+    public Tilemap tilemapVictory;
+    public Tilemap wallTilempa;
+    public Tilemap whiteTilemap;
+    public TilemapRenderer whiteTilemapRenderer;
+
+    public Camera cam;
+    public CinemachineVirtualCamera camPlayer;
+
+    [Header("Set up Player Movement")]
+    private Vector2 direction;
     public float moveSpeed;
     public float minDistance = 5;
 
-    
+    [Header("Victory Set up pour BlindPath")]
+    public float currentScore;
+    public float victoryScore;
 
+
+    [Header("Set up Vibrations Behaviour")]
     private RaycastHit2D leftRay;
     private RaycastHit2D rightRay;
     private RaycastHit2D upRay;
@@ -49,8 +60,6 @@ public class PlayerBehaviour : MonoBehaviour
     private void Awake()
     {
         raycastLayer = LayerMask.GetMask("Raycast");
-        //offset = GetComponent<BoxCollider2D>().size.x;
-        
     }
 
 
@@ -62,10 +71,27 @@ public class PlayerBehaviour : MonoBehaviour
         if(useVibration)
         {
             CheckVibration();
+
+            if(currentScore == victoryScore)
+            {
+                Debug.Log("Winner");
+            }
         }
         else
         {
             GamePad.SetVibration(PlayerIndex.One, 0, 0);
+
+            Bounds PCBounds = this.gameObject.GetComponent<Collider2D>().bounds;
+
+            Vector3 colliderFront = new Vector3(transform.position.x, PCBounds.max.y, PCBounds.max.z);
+            Vector3 colliderBack = new Vector3(transform.position.x, PCBounds.min.y, PCBounds.max.z);
+            Vector3 colliderLeftSide = new Vector3(PCBounds.min.x, transform.position.y, PCBounds.max.z);
+            Vector3 colliderRightSide = new Vector3(PCBounds.max.x, transform.position.y, PCBounds.max.z);
+
+            DestroyTile(colliderFront);
+            DestroyTile(colliderBack);
+            DestroyTile(colliderLeftSide);
+            DestroyTile(colliderRightSide);
         }
     }
 
@@ -140,11 +166,49 @@ public class PlayerBehaviour : MonoBehaviour
         GamePad.SetVibration(PlayerIndex.One, (upMotors + leftMotor), (rightMotor + upMotors));
     }
 
+    void DestroyTile(Vector3 pos)
+    {
+        Vector3Int tilePosition = tilemapVictory.WorldToCell(pos);
+
+        if (tilemapVictory.HasTile(tilePosition))
+        {
+            currentScore += 1;
+            tilemapVictory.SetTile(tilePosition, null);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        tilemapVictory.color = Color.green;
-        cam.backgroundColor = Color.white;
-        whiteTilemap.sortingOrder = -1;
+        if(collision.gameObject.layer == 0)
+        {
+            if(useVibration)
+            {
+                tilemapVictory.color = Color.green;
+                cam.backgroundColor = Color.white;
+                whiteTilemapRenderer.sortingOrder = -1;
+            }
+            else
+            {
+                
+            }
+        }
+        
+        if(collision.gameObject.layer == 5)
+        {
+            wallTilempa.color = Color.white;
+            whiteTilemap.color = Color.green;
+            camPlayer.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 5)
+        {
+            wallTilempa.color = Color.black;
+            tilemapVictory.color = Color.red;
+            camPlayer.gameObject.SetActive(true);
+        }
     }
 
 }
